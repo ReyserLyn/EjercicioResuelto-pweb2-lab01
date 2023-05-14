cb522dd61b1b3de169fbc9303197d61896f2e196
Reyser Julio Zapata Butron <rzapata@unsa.edu.pe>
2023-05-14 17:23:43 -0500

    Bienvenido al curso

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,7 +1,9 @@
 public class HolaMundo{
 	public static void main(String[] args){
 		// Codigo
-		
 		System.out.println("Hola mundo");
+
+		// Codigo 2
+		System.out.println("Bienvenido al Laboratorio de Programacion Web 2");
 	}
 }
